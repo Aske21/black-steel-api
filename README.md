8fc4f2c7679bfdaaf51969a654f5206f3bef55ec
Makii117 <MakiOnPc@gmail.com>
2022-04-04 19:26:11 +0200

    Server init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # black-steel-api
+
+
+https://github.com/airbnb/javascript
+
+https://github.com/airbnb/javascript
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-/*TODO :
- **  ADD FOLDER STRUCTURE
- */
+const express = require("express");
+const app = express();
+
+
+app.get("/", function (req, res) {
+    res.send('<h1>Hello</h1>');
+});
+
+
+//listen on port 5000
+app.listen(5000);
