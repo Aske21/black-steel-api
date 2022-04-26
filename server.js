fc725c3b772d4bdfb08c5a122b1184741e640a45
Aske21 <veledarevica@yahoo.com>
2022-04-27 00:03:37 +0200

    Making sure tests work

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
-
 app.get("/", function (req, res) {
-    res.send('<h1>Hello</h1>');
+  res.send("<h1>Hello</h1>");
 });
 
+app.get("/hello", function (req, res) {
+  res.send("hi");
+  res.status(200);
+});
 
 //listen on port 5000
 app.listen(5000);
--- test/test.js
+++ test/test.js
@@ -3,20 +3,34 @@ var should = require("should");
 
 var server = supertest.agent("http://localhost:5000");
 
-describe("SAMPLE unit test",function(){
-
-    // #1 should return home page
-    it("should return home page",function(done){
-      // calling home page
-      server
+describe("SAMPLE unit test", function () {
+  // #1 should return home page
+  it("should return home page", function (done) {
+    // calling home page
+    server
       .get("/")
-      .expect("Content-type",/text/)
+      .expect("Content-type", /text/)
       .expect(200) // THis is HTTP response
-      .end(function(err,res){
+      .end(function (err, res) {
         // HTTP status should be 200
         res.status.should.equal(200);
         done();
       });
-    });
-  
   });
+});
+
+describe("SAMPLE unit test", function () {
+  // #1 should return home page
+  it("routes check", function (done) {
+    // calling home page
+    server
+      .get("/hello")
+      .expect("Content-type", /text/)
+      .expect(200) // THis is HTTP response
+      .end(function (err, res) {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        done();
+      });
+  });
+});
